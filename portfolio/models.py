6bc233515c69a9dccae379da08ea4c00c691833f
rconaway-uno <41027252+rconaway-uno@users.noreply.github.com>
2018-07-23 20:02:17 -0500

    Updated Key

--- portfolio/models.py
+++ portfolio/models.py
@@ -72,7 +72,7 @@ class Stock(models.Model):
     def current_stock_price(self):
         symbol_f = str(self.symbol)
         main_api = 'https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols='
-        api_key = '&apikey=2EDATRIUDF644K9U'
+        api_key = '&apikey=SFKFI2CF7598S61O'
         url = main_api+symbol_f+api_key
         json_data = requests.get(url).json()
         open_price = json_data["Stock Quotes"][0]["2. price"]
